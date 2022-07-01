fdfab044c0597d388bd882f35a8b5e064bff1f4a
Erbol <a1darbekov640@gmail.com>
2022-07-01 11:43:01 +0300

    add backend addres

---
+++ src/api/adrress/controllers/adrress.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  adrress controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::adrress.adrress');
---
+++ src/api/adrress/services/adrress.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adrress service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::adrress.adrress');
